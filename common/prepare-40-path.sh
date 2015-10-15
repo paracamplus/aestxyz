41f88faca58d566828cb63503c30650b390812ab
Christian Queinnec <christian.queinnec@paracamplus.com>
2015-10-15 18:15:46 +0200

    Also generated json.

--- common/prepare-40-path.sh
+++ common/prepare-40-path.sh
@@ -3,12 +3,31 @@
 echo "Generating path.tt from YAML configuration"
 TODIR=$ROOTDIR/opt/$HOSTNAME
 mkdir -p $TODIR/Templates/doc
+mkdir -p $ROOTDIR/var/www/$HOSTNAME
+
+# Generate path.tt
 if ! $PARACAMPLUSDIR/Scripts/compilePath.pl --html \
     -i $TODIR/$HOSTNAME.yml \
     -d $PARACAMPLUSDIR/Servers/e/Paracamplus-FW4EX-E/exercisedir/ \
     -o $TODIR/Templates/doc/path.tt
 then 
     echo "Cannot generate $TODIR/path.tt"
+    exit 13
+fi
+
+# Generate JSON too. JSON are also stored on the E server.
+PATHDIR=$PARACAMPLUSDIR/Docker/vme/e.paracamplus.com/root.d
+PATHDIR=$PATHDIR/opt/e.paracamplus.com/path
+if $PARACAMPLUSDIR/Scripts/compilePath.pl --json \
+    -i $TODIR/$HOSTNAME.yml \
+    -d $PARACAMPLUSDIR/Servers/e/Paracamplus-FW4EX-E/exercisedir/ \
+    -o $ROOTDIR/var/www/$HOSTNAME/exercises.json
+then 
+    SHORTHOSTNAME=${HOSTNAME%.paracamplus.com}
+    cp -p $ROOTDIR/var/www/$HOSTNAME/exercises.json $PATHDIR/$SHORTHOSTNAME.json
+    cp -p $ROOTDIR/var/www/$HOSTNAME/exercises.json $PATHDIR/$HOSTNAME.json
+else
+    echo "Cannot generate $ROOTDIR/var/www/$HOSTNAME/exercises.json"
     exit 13
 fi
 
