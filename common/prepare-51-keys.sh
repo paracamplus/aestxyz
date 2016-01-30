9c0388e2098e1697e0a7ccedea59161ff5156a5f
Christian Queinnec <christian.queinnec@paracamplus.com>
2016-01-30 20:20:02 +0100

    Change dsa key to rsa.

--- common/prepare-51-keys.sh
+++ common/prepare-51-keys.sh
@@ -5,20 +5,18 @@
     cd $PARACAMPLUSDIR/Servers/.ssh
     tar czf $PARACAMPLUSDIR/Docker/${HOSTNAME}/keys.tgz \
         author* student*
-    # DSA keys to log as fw4ex on s.paracamplus.com:
+    # RSA keys to log as fw4ex on s.paracamplus.com:
     cp -p fw4ex fw4ex.pub $PARACAMPLUSDIR/Docker/${HOSTNAME}/
-    # DSA keys to log as saver on db.paracamplus.com:
-    cp -p saver_dsa saver_dsa.pub $PARACAMPLUSDIR/Docker/${HOSTNAME}/
+    # RSA keys to log as saver on db.paracamplus.com:
+    cp -p saver2_rsa saver2_rsa.pub $PARACAMPLUSDIR/Docker/${HOSTNAME}/
 
     # Identify the keys to be copied (by install.sh) into container's SSHDIR:
     cat > $PARACAMPLUSDIR/Docker/${HOSTNAME}/keys.txt <<EOF
 UNTAR keys.tgz
 FILE fw4ex
 FILE fw4ex.pub
-FILE saver_dsa
-FILE saver_dsa.pub
-FILE fw4ex
-FILE fw4ex.pub
+FILE saver2_rsa
+FILE saver2_rsa.pub
 EOF
 )
 
