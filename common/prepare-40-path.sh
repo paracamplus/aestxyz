b9fa0ad27300edf4fea00f4533b11d815b67829d
Christian Queinnec <christian.queinnec@paracamplus.com>
2016-02-12 14:11:29 +0100

    ajout verification YAML.

--- common/prepare-40-path.sh
+++ common/prepare-40-path.sh
@@ -6,11 +6,18 @@ mkdir -p $TODIR/Templates/doc
 mkdir -p $ROOTDIR/var/www/$HOSTNAME
 
 # Generate path.tt
-if ! $PARACAMPLUSDIR/Scripts/compilePath.pl --html \
+trap "rm /tmp/cp.log" 0
+if $PARACAMPLUSDIR/Scripts/compilePath.pl --html \
     -i $TODIR/$HOSTNAME.yml \
     -d $PARACAMPLUSDIR/Servers/e/Paracamplus-FW4EX-E/exercisedir/ \
-    -o $TODIR/Templates/doc/path.tt
+    -o $TODIR/Templates/doc/path.tt 2>&1 | tee /tmp/cp.log
 then 
+    if grep 'YAML Warning' < /tmp/cp.log
+    then
+        cat /tmp/cp.log
+        exit 13
+    fi
+else
     echo "Cannot generate $TODIR/path.tt"
     exit 13
 fi
@@ -18,14 +25,18 @@ fi
 # Generate JSON too. JSON are also stored on the E server.
 PATHDIR=$PARACAMPLUSDIR/Docker/vme/e.paracamplus.com/root.d
 PATHDIR=$PATHDIR/opt/e.paracamplus.com/path
+mkdir -p $PATHDIR
+JSON=$ROOTDIR/var/www/$HOSTNAME/exercises.json
 if $PARACAMPLUSDIR/Scripts/compilePath.pl --json \
     -i $TODIR/$HOSTNAME.yml \
     -d $PARACAMPLUSDIR/Servers/e/Paracamplus-FW4EX-E/exercisedir/ \
-    -o $ROOTDIR/var/www/$HOSTNAME/exercises.json
+    -o $JSON
 then 
     SHORTHOSTNAME=${HOSTNAME%.paracamplus.com}
-    cp -p $ROOTDIR/var/www/$HOSTNAME/exercises.json $PATHDIR/$SHORTHOSTNAME.json
-    cp -p $ROOTDIR/var/www/$HOSTNAME/exercises.json $PATHDIR/$HOSTNAME.json
+    cp -p $JSON $PATHDIR/$SHORTHOSTNAME.json
+    cp -p $JSON $PATHDIR/$HOSTNAME.json
+    cp -p $JSON $PARACAMPLUSDIR/Servers/e/path/$HOSTNAME.json
+    cp -p $JSON $PARACAMPLUSDIR/Servers/e/path/$SHORTHOSTNAME.json
 else
     echo "Cannot generate $ROOTDIR/var/www/$HOSTNAME/exercises.json"
     exit 13
