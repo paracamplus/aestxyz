0da2d871980e675acc56ba40b280759164055f5c
Christian Queinnec <christian.queinnec@paracamplus.com>
2016-03-04 18:21:16 +0100

    Increased wait time in check-10-endStart.sh

--- common/check-10-endStart.sh
+++ common/check-10-endStart.sh
@@ -1,14 +1,25 @@
 #! /bin/bash
 # This script is run in the Docker host.
-# Is start.sh finished ?
 
-if docker logs ${DOCKERNAME} 2>&1 | grep -Eq '^=+ End of start.sh'
-then
-    true
-else
-    echo "Docker image ${DOCKERNAME} not ready !?"
-    docker logs ${DOCKERNAME} 2>&1 | tail -100
-    exit 58
-fi
+echo "Check whether start.sh is finished ?"
+
+for i in 1 2 3 4 5 fin
+do
+    if [ "$i" = fin ]
+    then
+        echo "Docker image ${DOCKERNAME} not at all ready !?"
+        docker logs ${DOCKERNAME} 2>&1 | tail -100
+        exit 58
+    else
+        # Leave time to let docker logs to flush...
+        sleep 5
+        if docker logs ${DOCKERNAME} 2>&1 | grep -Eq '^=+ End of start.sh'
+        then
+            break
+        else
+            echo "Docker image ${DOCKERNAME} not yet($i) ready !?"
+        fi
+    fi
+done
 
 # end of check-10-endStart.sh
